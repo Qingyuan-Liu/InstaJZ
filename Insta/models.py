8b39f1617f3057f91c105bfb1bd7dfe5bf3558f9
Qingyuan-Liu <qyyf@umich.edu>
2020-05-18 01:52:41 -0400

    Backend and frontend interation

--- Insta/models.py
+++ Insta/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+from django.urls import reverse
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+# all class should inherit models.Model
+
+
+
+class InstaUser(AbstractUser):
+    profile_pic=ProcessedImageField(
+        upload_to='static/images/profile',
+        format='JPEG',
+        options={'quality':100},
+        blank=True,
+        null=True
+    )
+
+class Post(models.Model):
+    author=models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='my_posts'
+    )
+    title=models.TextField(blank=True, null=True)
+    image=ProcessedImageField(
+        upload_to='static/images/posts',
+        format='JPEG',
+        options={'quality':100},
+        blank=True,
+        null=True,
+    )
+    #当有人创建新的post,或者对post进行修改，就会调用getabsoluteurl
+    #当有人创建了新的post就会reverse到url.py中的name为helloworld对应的url
+    #reverse:"helloworld"-->"www.hellowordl.com"将string反转成url
+    def get_absolute_url(self):
+        return reverse("post_detail",args=[str(self.id)])
+    def get_like_count(self):
+        return self.likes.count()
+     
+
+
+
+class Like(models.Model):
+    post = models.ForeignKey(
+        Post, 
+        on_delete=models.CASCADE,
+        related_name='likes')
+    user = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+    #解释related_name='likes'
+    #like1--> wentailai like post1
+    #like2--> test like post1
+    #当调用post1.likes-->(like1,like2)
+    #wentailai.likes-->like1
+
+    class Meta:
+        unique_together=("post","user")
+        #同一个user和同一个post只能被定义一次
+    
+    def __str__(self):
+        return 'Like:'+self.user.username+' likes '+self.post.title
+
+        
+
+    
--- Insta/views.py
+++ Insta/views.py
@@ -1,6 +1,60 @@
+from annoying.decorators import ajax_request
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView,DetailView
+from .models import Post,Like
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse,reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from Insta.forms import CustomUserCreationForm
 
 class HelloWorld(TemplateView):
     template_name='test.html'
 
+class PostsView(ListView):
+    model=Post
+    template_name='index.html'
+
+class PostDetailView(DetailView):
+    model=Post
+    template_name='post_detail.html'
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model=Post
+    template_name='post_create.html'
+    fields='__all__'
+    login_url='login'
+
+class PostUpdateView(UpdateView):
+    model=Post
+    template_name='post_update.html'
+    fields=['title']
+
+class PostDeleteView(DeleteView):
+    model=Post
+    template_name='post_delete.html'
+    #删除时需要用reverselazy，可以让delete之后再跳转
+    success_url=reverse_lazy('posts')
+
+class SignUp(CreateView):
+    form_class=CustomUserCreationForm
+    template_name='signup.html'
+    success_url=reverse_lazy("login")
+
+@ajax_request
+def addLike(request):
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    try:
+        like = Like(post=post, user=request.user)
+        like.save()
+        result = 1
+    except Exception as e:
+        like = Like.objects.get(post=post, user=request.user)
+        like.delete()
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk
+    }
