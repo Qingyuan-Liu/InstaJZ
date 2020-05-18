3dcfb6b9d628aaa4112b22d25530b83d77c3b06d
Qingyuan Liu <54901758+Qingyuan-Liu@users.noreply.github.com>
2020-05-18 03:24:22 -0400

    Update urls.py

--- Insta/urls.py
+++ Insta/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='make_post'),
     path('post/update/<int:pk>', PostUpdateView.as_view(), name='post_update'),
     path('post/delete/<int:pk>', PostDeleteView.as_view(), name='post_delete'),
-    path('like', addLike, name='addLike'),
+    path('like/', addLike, name='addLike'),
 ]
